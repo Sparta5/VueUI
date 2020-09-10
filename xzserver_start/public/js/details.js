c42fe704bbbd69b1225b4f48904b84a6de564313
bing <bing@123.com>
2020-09-11 00:00:51 +0800

    9/11am

--- xzserver_start/public/js/details.js
+++ xzserver_start/public/js/details.js
@@ -27,4 +27,32 @@ ajax("get","/details",`lid=${lid}`).then(result=>{
       html+=`<a class="btn btn-sm btn-outline-secondary ${sp.lid==lid?"active":""}" href="product_details.html?lid=${sp.lid}">${sp.spec}</a>`
     }
     document.querySelector("#details>div:nth-child(5)>div:nth-child(2)").innerHTML=html;
+    /*填充下方的商品配置的详情*/  
+    var html=`
+    <li class="float-left mb-2"><a class="text-muted small" href="#">商品名称：${product.lname}</a></li>
+    <li class="float-left mb-2"><a class="text-muted small" href="#">系统：${product.os}</a></li>
+    <li class="float-left mb-2"><a class="text-muted small" href="#">内存容量：${product.memory}</a></li>
+    <li class="float-left mb-2"><a class="text-muted small" href="#">分辨率：${product.resolution}</a></li>
+    <li class="float-left mb-2"><a class="text-muted small" href="#">显卡型号：${product.video_card}</a></li>
+    <li class="float-left mb-2"><a class="text-muted small" href="#">处理器：${product.subtitle}</a></li>
+    <li class="float-left mb-2"><a class="text-muted small" href="#">显存容量：${product.video_memory}</a></li>
+    <li class="float-left mb-2"><a class="text-muted small" href="#">分类：${product.category}</a></li>
+    <li class="float-left mb-2"><a class="text-muted small" href="#">硬盘容量:${product.disk}</a></li>
+`;
+document.querySelector("#params>div>div>div>ul")
+                      .innerHTML=html;
+document.querySelector("#params>div>div")
+         .children[5]
+         .innerHTML=product.details;
+    /*****实现放大镜效果*****/
+    var html=``;
+    for(var p of pics){
+      html+=`<li class="float-left p-1">
+      <img src="${p.sm}" data-md="${p.md}" data-lg="${p.lg}">
+    </li>`
+    }
+    var ulImgs=document.querySelector
+    ("#preview>div>div:last-child ul");
+    ulImgs.innerHTML=html;
+    ulImgs.style.width=LIWIDTH*pics.length+"px"
 })
