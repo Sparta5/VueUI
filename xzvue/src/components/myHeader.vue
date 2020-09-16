442048648ea0de11ae524238605ce54bc27ce759
binbin <chenbin@qq.com>
2020-09-16 21:00:49 +0800

    9/16

---
+++ xzvue/src/components/myHeader.vue
@@ -0,0 +1,11 @@
+<template>
+  <div id="nav">
+    <router-link to="/">首页</router-link>
+    <router-link to="/details/5">详情页</router-link>
+  </div>
+</template>
+<script>
+export default {//在/main.js中new Vue()之前,把这个组件变成全局组件
+  
+}
+</script>
--- xzvue/src/router/index.js
+++ xzvue/src/router/index.js
@@ -1,27 +1,23 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Index from '../views/Index'
 import Details from '../views/Details'
 Vue.use(VueRouter)
-
+// 路由字典
 const routes = [
   {
     path: '/',
-    name: 'home',
-    component: Home
-  },
-  {
-    path:"/details/:lid",
-    component:Details,
-    props:true//有参数参数，必须加上props为true
+    name: 'Index',
+    component: Index//匹配地址栏和文件夹
   },
   { //这里是懒加载，暂时不要问，明天讲
-    path: '/about',
-    name: 'about',
+    path:"/details/:lid",
+    props:true,//有参数参数，必须加上props为true
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    //异步加载要改两个地方
+    component: () => import(/* webpackChunkName: "detalis" */ '../views/Details.vue')
   }
 ]
 
