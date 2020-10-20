9818f7fe6bd2a14301b73b7be9b02fb5f7f4e6d3
bing <bing@123.com>
2020-10-21 01:54:54 +0800

    10/21

--- VueX/resource/HTML5/day05/01/worker.js
+++
@@ -1,8 +0,0 @@
-this.addEventListener('message',(e) => {
-   let numberVal = parseInt(e.data);
-   let sum = 0;
-   for (let n =0;n<=numberVal;n++) {
-     sum += n;
-   }
-   this.postMessage(sum)
-})
