ba70cf951c4f8f79d41896b2c8e44824d667a385
chenbin <chenbin@123.com>
2020-11-06 21:02:38 +0800

    11/6

--- react_app/src/App.js
+++ react_app/src/App.js
@@ -1,38 +1,48 @@
 import React, { Component } from 'react'
 
 export default class App extends Component {
-  state={
-    uname:'',
-    age:0
-  }
+  state={ uname:"", age:"", desc:""};
 
+  _nameChanged = (event) =>{
+    this.setState({ uname: event.target.value });
+  }
   render() {
     return (
       <div>
-        姓名：<input value={this.state.uname} onChange={this._doUame} type="text"/><br/>
-        年龄：<input
-               type="text"
-               valuef={this.state.uname}
+        姓名：
+        <input
+            value={this.state.uname} 
+            onChange={this._nameChanged} 
+            type="text"/><br/>
+        年龄：
+        <input
+            type="text"
+            value={this.state.age}
+            onChange={(event) => this.setState({ age:event.target.value })}
+            /><br/>
+        {/* 普通函数，用bin 绑定this */}
+        <button onClick={this._doSure.bind(this)}>确定</button>
+        <p 
+          style={{
+            color: "blue",
+            fontSize:"25px",
+            backgroundColor:"orange",
+            fontWeight:"bold"
+          }} 
+        >
+          {this.state.desc}
+        </p>
 
-                /><br/>
-        <button>确定</button>
-        <p>{this.state.uname}今年{this.state.age}</p>
+        <p className="desc">{this.state.desc}</p>
       </div>
       
     )
   }
 
-  _doUame = (event) =>{
-    console.log(event);
-
-    let uname = event.target.value;
-    this.setState({uname})
-  }
-
   _doSure() {
 
-    let { name, age } = this.state;
+    let { uname, age } = this.state;
 
-    this.setState({ desc: `${name}` })
+    this.setState({ desc: `${uname}今年${age}岁`});
   }
 }
